086864c648b9921d441f42e2b379ac27e625fd8b
Milo Bonnet <milovtt@gmail.com>
2022-12-01 11:52:17 +0100

    Base

---
+++ script/script.js
@@ -0,0 +1,44 @@
+const collection = [
+    {
+      name: "Me Time",
+      director: "John Hamburg",
+      releaseYear: 2022,
+      picture: "https://images2.minutemediacdn.com/image/fetch/w_2000,h_2000,c_fit/https%3A%2F%2Fnetflixlife.com%2Ffiles%2Fimage-exchange%2F2022%2F08%2Fie_91253.jpeg",
+      genre: ["Comedie"],
+      cast: [
+        "Kevin Hart",
+        "Mark Wahlberg",
+        "Regina Hall",
+      ],
+    },
+
+    {
+        name: "Yes Day",
+        director: "Miguel Arteta",
+        releaseYear: 2021,
+        picture: "https://fr.web.img6.acsta.net/r_654_368/newsv7/21/03/12/16/23/0397816.jpg",
+        genre: ["Comedie", "Famille"],
+        cast: [
+          "Jenna Ortega",
+          "Jennifer Garner",
+          "Édgar Ramírez",
+        ],
+      },
+
+      {
+        name: "Loin du périph",
+        director: "Louis Leterrier",
+        releaseYear: 2022,
+        picture: "https://www.journaldugeek.com/content/uploads/2022/05/lp4.jpg",
+        genre: ["Comedie", "Action"],
+        cast: [
+          "Omar Sy",
+          "Izïa Higelin",
+          "Laurent Lafitte",
+        ],
+      },
+  
+    // ..
+  ];
+  
+
